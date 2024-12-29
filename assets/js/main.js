7bd536215bd6199e2a5a21792eaf7d4c6ac465d5
acconcltv <accon.cltve@gmail.com>
2024-12-29 20:55:38 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,31 @@ module.exports = {
     },
     plugins: [],
   };
+
   
+  document.querySelectorAll('a[href^="#event"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = 100;
+      const top = target.getBoundingClientRect().top + window.scrollY - offset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    });
+  });
+  document.querySelectorAll('a[href^="#gallery"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = 100; 
+      const top = target.getBoundingClientRect().top + window.scrollY - offset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    });
+  });
+
+  document.querySelectorAll('a[href^="index.html#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetUrl = this.getAttribute('href');
+      window.location.href = targetUrl;
+    });
+  });
